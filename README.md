e727202cd3c1861391b798f1efa57b5d1d6d467b
pi <ravjan07@gmail.com>
2017-06-30 12:51:23 +0000

    Tomcat8 reboot

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Hello world
 http://www.yougetsignal.com/tools/open-ports/
 su - USER_FOOBAR -c /PATH/TO/MY_APP &
+sudo touch tomcat8
+sudo chmod 755 /etc/init.d/tomcat8 
+sudo update-rc.d tomcat8 defaults 
+sudo service tomcat8 stop
+sudo service tomcat8 restart
--- src/main/java/jdev/Server.java
+++ src/main/java/jdev/Server.java
@@ -4,15 +4,20 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server extends Thread {
+    private static int port = 8000;
 
     public static void main(String[] args) {
+	if (args != null && args.length > 0){
+		port = Integer.parseInt(args[0]);
+	}
         new Server().start();
     }
 
     @Override
     public void run() {
-        try (ServerSocket server = new ServerSocket(8000)) {
-            while (true) {
+        try (ServerSocket server = new ServerSocket(port)) {
+            System.out.println("Server is running on " + port);
+	    while (true) {
                 Socket client = server.accept();
                 System.out.println("New Client " + client.getInetAddress());
                 client.getOutputStream().write("Hello from pi server!".getBytes());
